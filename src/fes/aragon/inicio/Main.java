5fdad082422ce6430b6a70a2b36374805a188129
Dani-Ideas <daniideas0@gamil.com>
2023-10-20 08:27:46 -0600

    add the second buttom in ui

--- src/fes/aragon/inicio/Main.java
+++ src/fes/aragon/inicio/Main.java
@@ -3,7 +3,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.Pane; 	
 import javafx.fxml.FXMLLoader;
 public class Main extends Application {
 @Override
